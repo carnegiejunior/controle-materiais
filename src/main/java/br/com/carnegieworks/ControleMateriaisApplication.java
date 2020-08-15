eef9a04468093aa6ae2bde833df890ed78cd14a2
Carnegie <carnegiejunior@gmail.com>
2020-08-15 17:14:17 -0300

    Bootstrap

---
+++ src/main/java/br/com/carnegieworks/ControleMateriaisApplication.java
@@ -0,0 +1,13 @@
+package br.com.carnegieworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleMateriaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleMateriaisApplication.class, args);
+	}
+
+}
